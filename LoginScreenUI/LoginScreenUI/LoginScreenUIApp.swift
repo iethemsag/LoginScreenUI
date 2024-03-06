a0c6cc9d596ce9848f07c92c3cd4ee47a5d47933
Ibrahim Ethem Sag <sagibrahimethem@gmail.com>
2024-03-06 21:11:19 +0300

    LoginScreenUI

---
+++ LoginScreenUI/LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Ibrahim Ethem Sag on 6.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
